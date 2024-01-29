5ed42420e974509ff83499ad90a04e3a5d5dda87
Onome Ogwuda <onomeogwuda@gmail.com>
2024-01-29 17:23:40 +0000

    update sign up form

--- src/pages/RegistrationPage/RegistrationPage.js
+++ src/pages/RegistrationPage/RegistrationPage.js
@@ -7,22 +7,40 @@ const RegistrationPage = () => {
       <div className="registration">
         <h2 className="registration__title">Tell us more about you</h2>
         <form className="registration__form">
-          <label htmlFor="first-name">First Name</label>
+          <label htmlFor="first-name" className="registration__form-label">
+            First Name
+          </label>
           <input type="text" id="first-name" />
-          <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" />
-          <label htmlFor="dob">Date of Birth</label>
-          <input type="text" id="dob" />
-          <label htmlFor="number">Phone Number</label>
-          <input type="number" id="number" />
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" />
+          <label htmlFor="last-name" className="registration__form-label">
+            Last Name
+          </label>
+          <input
+            type="text"
+            id="last-name"
+            className="registration__form-input"
+          />
+          <label htmlFor="dob" className="registration__form-label">
+            Date of Birth
+          </label>
+          <input type="text" id="dob" className="registration__form-input" />
+          <label htmlFor="number" className="registration__form-label">
+            Phone Number
+          </label>
+          <input
+            type="number"
+            id="number"
+            className="registration__form-input"
+          />
+          <label htmlFor="email" className="registration__form-label">
+            Email
+          </label>
+          <input type="email" id="email" className="registration__form-input" />
           <div className="registration__form-btns">
-            <Link to="/dashboard" className="registration__form-btn">
-              Submit
-            </Link>
             <Link to="/login" className="registration__form-btn">
               Login
+            </Link>
+            <Link to="/signup/biometrics" className="registration__form-btn">
+              Next
             </Link>
           </div>
         </form>
