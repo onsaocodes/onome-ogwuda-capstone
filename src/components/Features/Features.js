5006cc49e2ba00b5f6cf0adfc3186f176d637812
Onome Ogwuda <onomeogwuda@gmail.com>
2024-01-29 16:39:05 +0000

    create feature cards

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -1,10 +1,51 @@
 import "./Features.scss";
+import dumbbells from "../../assests/images/dumbbells.jpg";
+import food from "../../assests/images/food.jpg";
+import graph from "../../assests/images/graph.jpg";
 
 const Features = () => {
   return (
     <div className="features">
       <div className="features__heading">
-        <h3 className="features__title">Features</h3>
+        <h2 className="features__title">Features</h2>
+      </div>
+      <div className="features__container">
+        <div className="features__card">
+          <img
+            src={dumbbells}
+            alt="Rack of dumbbells"
+            className="features__card-image"
+          />
+          <div className="features__card-container">
+            <h4 className="features__card-title">
+              Bespoke workouts tailored to your goals
+            </h4>
+          </div>
+        </div>
+        <div className="features__card">
+          <img
+            src={food}
+            alt="Rack of dumbbells"
+            className="features__card-image"
+          />
+          <div className="features__card-container">
+            <h4 className="features__card-title">
+              Meals you will actually enjoy - we don't cut out any food groups
+            </h4>
+          </div>
+        </div>
+        <div className="features__card">
+          <img
+            src={graph}
+            alt="Rack of dumbbells"
+            className="features__card-image"
+          />
+          <div className="features__card-container">
+            <h4 className="features__card-title">
+              Keep track of your progress
+            </h4>
+          </div>
+        </div>
       </div>
     </div>
   );
