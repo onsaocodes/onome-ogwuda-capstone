a45103244a86270045fe86b2f81097217b2b1a71
Onome Ogwuda <onomeogwuda@gmail.com>
2024-01-30 11:00:45 +0000

    Create route for biometrics

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import "./App.scss";
 import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
+import BiometricsPage from "./pages/BiometricsPage/BiometricsPage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Routes>
             <Route path="/" element={<LandingPage />} />
             <Route path="/signup" element={<RegistrationPage />} />
+            <Route path="/biometrics" element={<BiometricsPage />} />
           </Routes>
         </main>
       </BrowserRouter>
---
+++ src/pages/BiometricsPage/BiometricsPage.js
@@ -0,0 +1,56 @@
+import "./BiometricsPage.scss";
+import { Link } from "react-router-dom";
+
+const BiometricsPage = () => {
+  return (
+    <>
+      <div className="registration">
+        <h2 className="registration__title">Tell us more about you</h2>
+        <form className="registration__form">
+          <label htmlFor="first-name" className="registration__form-label">
+            First Name
+          </label>
+          <input
+            type="text"
+            id="first-name"
+            className="registration__form-input"
+          />
+          <label htmlFor="last-name" className="registration__form-label">
+            Last Name
+          </label>
+          <input
+            type="text"
+            id="last-name"
+            className="registration__form-input"
+          />
+          <label htmlFor="dob" className="registration__form-label">
+            Date of Birth
+          </label>
+          <input type="text" id="dob" className="registration__form-input" />
+          <label htmlFor="number" className="registration__form-label">
+            Phone Number
+          </label>
+          <input
+            type="number"
+            id="number"
+            className="registration__form-input"
+          />
+          <label htmlFor="email" className="registration__form-label">
+            Email
+          </label>
+          <input type="email" id="email" className="registration__form-input" />
+          <div className="registration__form-btns">
+            <Link to="/login" className="registration__form-btn">
+              Login
+            </Link>
+            <Link to="/signup/biometrics" className="registration__form-btn">
+              Next
+            </Link>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default BiometricsPage;
