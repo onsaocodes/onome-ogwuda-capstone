5af20b702190df0b2579b4e25af447681339738b
Onome Ogwuda <onomeogwuda@gmail.com>
2024-02-01 17:44:55 +0000

    display user name

---
+++ src/pages/UserDashboardPage/UserDashboardPage.js
@@ -0,0 +1,43 @@
+import "./UserDashboardPage.scss";
+import { Link, useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const UserDashboardPage = () => {
+  const [user, setUser] = useState(null);
+  let { id } = useParams();
+
+  const URL = process.env.REACT_APP_URL;
+  const PORT = process.env.REACT_APP_PORT;
+
+  console.log(`${URL}${PORT}/${id}`);
+
+  const getUser = async () => {
+    try {
+      const response = await axios.get(`${URL}${PORT}/users/${id}`);
+      setUser(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  if (!user) {
+    return <section className="user-absent">User not found...</section>;
+  }
+
+  return (
+    <>
+      <div className="user">
+        <div className="user__title-container">
+          <h2 className="user__title">Welcome {user.first_name}</h2>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UserDashboardPage;
